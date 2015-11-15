1450ebe8ff1d5e6c9b6fa7b42d4e7bb2ae049874
Siena <siena.martinez@gmail.com>
2015-11-15 20:38:33 +0100

    Adding comments to the script

--- aviones.sh
+++ aviones.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-#Creamos el listado de motores
+# El script muestra el número de aviones que hay con un determinado número de motores
+
+# El primer parámetro es el nombre del fichero del que queremos tomar los datos
+# El segundo parámetro es para introducir el número de motores
 csvsort -d '^' -c nb_engines $1 -r| csvcut -c nb_engines | csvgrep -c nb_engines -m $2 |tail -n +2  | wc -l
